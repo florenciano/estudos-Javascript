66b24533845e3651388b969e67e2e5f3f2f01a65
florenciano <florenciano.rodrigo@gmail.com>
2013-11-17 20:55:54 -0200

    noções de orientação a objeto

---
+++ orientacaoObjeto/main.js
@@ -0,0 +1,7 @@
+
+var sacola1 = new Sacola();
+var sacola2 = new Sacola();
+
+console.log(sacola1.obterTotal());
+
+console.log(sacola2.meuValorTotal(200));
---
+++ orientacaoObjeto/sacola.js
@@ -0,0 +1,26 @@
+/* função como classe */
+
+
+// Sacola.js
+function Sacola() {
+	"use strict";
+
+	// disponivel somente nesta func
+	var total = 0;
+
+	// ENCAPSULAMENTO: para obter o valor da variavel total nos outros obj
+	this.obterTotal = function() {
+
+		return total;
+
+	};
+
+	// determinando o valor que gostariamos que tivesse o total
+	// pense que é possível usar func para trabalhar este valor
+	this.meuValorTotal = function(param) {
+
+		total = param;
+		return total;
+
+	};
+}
