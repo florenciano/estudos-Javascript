6d89f7f5c9f1e52d73fa845e71f46e33223e6b91
florenciano <florenciano.rodrigo@gmail.com>
2013-12-27 14:28:35 -0200

    null

---
+++ searchAngular/js/controllers.js
@@ -0,0 +1,27 @@
+// vide example
+// http://angular.github.io/angular-phonecat/step-3/app/
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+	$scope.phones = [
+	{	'category'	: 'blackboard',
+		'name'		: 'Nexus S',
+		'snippet'	: 'Fast just got faster with Nexus S.',
+		'snippet2'	: 'Lorem ipsum.',
+		'snippet3'	: 'Lorem ipsum dolor.',
+		'snippet4'	: 'Lorem ipsum dolor sit.',
+		'snippet5'	: 'Lorem ipsum dolor sit amet.'},
+
+	{	'category'	: 'rubric',
+		'name'		: 'Motorola XOOM™ with Wi-Fi',
+		'snippet'	: 'The Next, Next Generation tablet.',
+		'snippet2'	: 'Lorem ipsum.' },
+
+	{	'category'	: 'catme',
+		'name'		: 'MOTOROLA XOOM™',
+		'snippet'	: 'The Next, Next Generation mobile.',
+		'snippet2'	: 'Lorem ipsum.',
+		'snippet3'	: 'Lorem ipsum dolor.' }
+	];
+});
---
+++ searchAngular/js/limpeza.js
@@ -0,0 +1,23 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	APAGANDO OS LI VAZIOS
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+function clearList(event) {
+	// varre todos os listas
+	var list = document.querySelectorAll(".sublist");
+	for(var i = 0; i < list.length; i++) {
+		var contentLink = list[i].firstChild;
+		// verifica-se os links tem conteúdo de texto
+		if(contentLink.textContent == "" || contentLink.textContent == null || contentLink.textContent == undefined) {
+			list[i].style.display="none";
+		}
+	}
+}
+// ao carregar a pagina
+$(document).ready(function(){
+	clearList();
+});
+// e também quando digitar no input e perder o foco do mesmo
+$( "#pesquisar" ).bind("keydown", "blur",function(){
+	clearList();
+});
