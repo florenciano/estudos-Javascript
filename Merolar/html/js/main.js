e571073006a6b7510c0b4ddd913323779f06930a
florenciano <florenciano.rodrigo@gmail.com>
2014-12-14 09:23:40 -0200

    merolar - finished

--- Merolar/html/js/main.js
+++ Merolar/html/js/main.js
@@ -56,26 +56,28 @@ $(document).ready(function () {
 
     /* INDEX */
 
-    // hide ico 'search'
+    /////////////////////////////////////////////////////////////////////
+    // hide ico 'search' header
+    /////////////////////////////////////////////////////////////////////
     var search = $( "#search" ), icoSearch = $( ".icoSearch" );
     search.on( "focus, focusin", function() { icoSearch.hide() });
     search.on( "blur, focusout", function() { icoSearch.css( "display","inline-block" ) });
 
+    /////////////////////////////////////////////////////////////////////
     // carousel
+    /////////////////////////////////////////////////////////////////////
     var carouselHeader = $( ".carouselHeader" ).bxSlider({
         pagerSelector: '.pagerCustomizado'
     });
     // destroy carousel in mobile
-    if( $(window).width() < 481 ) {   
+    if( $(window).width() < 481 ) {
         carouselHeader.destroySlider();
     }
     $(window).resize(function() {
-        if( $(this).width() < 481 ) {   
+        if( $(this).width() < 481 ) {
             carouselHeader.destroySlider();
         }
     });
-
-
     // adjust alinhament elements carousel header
     function adjusAlign(el) {
         var p = $(el);
@@ -86,55 +88,119 @@ $(document).ready(function () {
 
     /* EMPREENDIMENTOS */
 
-    // thumbnails gallery
+    /////////////////////////////////////////////////////////////////////
+    // thumbnails of the section 'galeria'
+    /////////////////////////////////////////////////////////////////////
+    // galeria-1
     $( ".carouselThumb" ).bxSlider({
         minSlides: 3,
-        maxSlides: 4,
-        slideMargin: 10,
-        slideWidth: 170, // alterar larguras das imagens
+        maxSlides: 3,
+        slideWidth: 150, // alterar larguras das imagens
         pager: false,
         nextSelector: ".nextGaleria",
         prevSelector:  ".prevGaleria"
     });
-    // aplication in the images of the gallery
-    // used thumbnails above
     $( ".pagerThumb" ).bxSlider({
-        pagerCustom: "#carouselThumbPager"
+        pagerCustom: "#carouselThumbPager",
+        controls: false
     });
 
-    // swipe images of the 'plantas'
-    var containerImg = $( ".imgGaleriaPlantas" ), botoesImg = $( ".btnPlantas" );
-    botoesImg.each( function (key, value) {
-        $(this).on( "click", function (ev) {
-            $(containerImg).fadeOut( 'fast' );
-            ev.preventDefault();
-            if( key == 0 ) { $(containerImg).eq(0).fadeIn( 'fast' ); }
-            if( key == 1 ) { $(containerImg).eq(1).fadeIn( 'fast' ); }
-            if( key == 2 ) { $(containerImg).eq(2).fadeIn( 'fast' ); }
-            if( key == 3 ) { $(containerImg).eq(3).fadeIn( 'fast' ); }
+    // galeira-2
+    $( ".carouselThumb-2" ).bxSlider({
+        minSlides: 3,
+        maxSlides: 3,
+        slideWidth: 150, // alterar larguras das imagens
+        pager: false,
+        nextSelector: ".nextGaleria-2",
+        prevSelector:  ".prevGaleria-2"
+    });
+    $( ".pagerThumb-2" ).bxSlider({
+        pagerCustom: "#carouselThumbPager-2",
+        controls: false
+    });
+
+    // galeira-3
+    $( ".carouselThumb-3" ).bxSlider({
+        minSlides: 3,
+        maxSlides: 3,
+        slideWidth: 150, // alterar larguras das imagens
+        pager: false,
+        nextSelector: ".nextGaleria-3",
+        prevSelector:  ".prevGaleria-3"
+    });
+    $( ".pagerThumb-3" ).bxSlider({
+        pagerCustom: "#carouselThumbPager-3",
+        controls: false
+    });
+
+    // galeira-4
+    $( ".carouselThumb-4" ).bxSlider({
+        minSlides: 3,
+        maxSlides: 3,
+        slideWidth: 150, // alterar larguras das imagens
+        pager: false,
+        nextSelector: ".nextGaleria-4",
+        prevSelector:  ".prevGaleria-4"
+    });
+    $( ".pagerThumb-4" ).bxSlider({
+        pagerCustom: "#carouselThumbPager-4",
+        controls: false
+    });
+    // swipe galeria
+    var imgDasGalerias = $( ".galerias" ),
+        botoesGaleria = $( ".btnGaleria" );
+    swipeImage(imgDasGalerias, botoesGaleria);
+
+    // add style 'active'
+    $(botoesGaleria).click(function(){
+        $(botoesGaleria).removeClass( "activeBtnGaleria" );
+        $(this).toggleClass( "activeBtnGaleria" );
+    });
+
+    /////////////////////////////////////////////////////////////////////
+    // swipe images of the section 'plantas'
+    /////////////////////////////////////////////////////////////////////
+    function swipeImage (elContainer, btnClick) {
+        $(btnClick).each( function (key, value) {
+            $(this).on( "click", function (ev) {
+                $(elContainer).fadeOut( 'fast' );
+                if( key == 0 ) { $(elContainer).eq(0).fadeIn( 'fast' ); } // equivale a cada btn na página
+                if( key == 1 ) { $(elContainer).eq(1).fadeIn( 'fast' ); }
+                if( key == 2 ) { $(elContainer).eq(2).fadeIn( 'fast' ); }
+                if( key == 3 ) { $(elContainer).eq(3).fadeIn( 'fast' ); }
+                ev.preventDefault();
+            });
         });
+    }
+    var containerImg = $( ".imgGaleriaPlantas" ),
+        botoesImg = $( ".btnPlantas" );
+    swipeImage (containerImg, botoesImg);
+
+    // add style 'active'
+    $(botoesImg).click(function(){
+        $(botoesImg).removeClass( "activeBtnGaleria" );
+        $(this).toggleClass( "activeBtnGaleria" );
     });
 
-    // progress work construcion
+    /////////////////////////////////////////////////////////////////////
+    // progress 'Estagio da Obra'
+    /////////////////////////////////////////////////////////////////////
     function progressBar() {
         var progressBar = $( ".percent" ),
             display = $( ".barra span" );
 
-
-        progressBar.each(function() {
+        progressBar.each(function (key,value) {
             // attr 'data-value' ao progressBar
             var value = $(this).attr( "data-value" );
             $(this).css( "width", value + "%" );
-
-            // var max = $(this).attr( "data-max" );
 
             // set text percentual
             var display = $(this).prev( "span" );
             display.html( value + "%" );
 
             // conditional display's
-            if( value >= 75 ) { display.css( "color", "#fff" ) };
-            if( value > 100 ) { 
+            if( value >= 55 ) { display.css( "color", "#fff" ) };
+            if( value > 100 ) {
                 $(this).css( "width", "100%" );
                 display.html( "100%" );
             };
@@ -142,7 +208,15 @@ $(document).ready(function () {
         });
     }
     progressBar();
-    
+
+    /////////////////////////////////////////////////////////////////////
+    // thumbnails 'Estagio da obra' section 'Fotos retiradas na obra'
+    /////////////////////////////////////////////////////////////////////
+    $( ".imgSlider" ).bxSlider({
+        pagerCustom: "#pagerSlides",
+        controls: false
+    });
+
 
 });
 
