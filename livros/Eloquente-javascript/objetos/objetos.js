a86fa9c95c5ec1d6609e4d45db2ce0eada4bb1f2
Rodrigo Florencano <florenciano.rodrigo@gmail.com>
2016-03-10 09:29:20 -0300

    objects, methods, prototype and exercise

---
+++ livros/Eloquente-javascript/objetos/objetos.js
@@ -0,0 +1,186 @@
+(function() {
+	
+	/*
+		MÉTODOS
+		Propriedades simples que comportam valores de funções.
+		A variável especial this aponta para o objeto do qual foi chamada
+	*/
+
+	// método
+	function speak (line) {
+		console.log("The " + this.type + " rabbit says '" + line + "'.");
+	}
+
+	// objeto 1
+	var whiteRabbit = {
+		type: "white",
+		speak: speak
+	}
+	// objeto 2
+	var fatRabbit = {
+		type: "fat",
+		speak: speak
+	}
+
+	// call método
+	whiteRabbit.speak("Oh my ears and whiskers");
+
+	/* Aqui 'Call' e 'Apply' chamam uma fn que também é um método */
+	speak.apply(fatRabbit, ["Burp!"]);
+	speak.call({type: "old"}, "Oh my");
+
+	/*
+		PROTOYPE
+		Conceito na prática
+	*/
+
+	var protoRabbit = {
+		speak : function(frase) {
+			console.log("The rabbit " + this.type + " say '" + frase + "'.");
+		}
+	}
+	var rabbitKiller = Object.create(protoRabbit);
+	rabbitKiller.type = "killer";
+	rabbitKiller.speak("SKREEEEE!"); // The rabbit killer say 'SKREEEE!'
+
+	/*
+		CONSTRUTORES
+		Conceito na prática
+	*/
+	function Cat(type) { // por convenção utiliza a inicial em Maiúscula
+		this.type = type;
+	}
+	var catYellow = new Cat("Sleeper!");
+	console.log(catYellow); // Cat {type: 'Sleeper!'}
+
+	Cat.prototype.fala = function(frase) {
+		console.log("The cat is um motherfocker and is '" + this.type + "' " + frase);
+	};
+	catYellow.fala("Doom");
+
+	/*
+		PROGRAMA
+		Criar tabela
+		Neste exemplo sera utilizado fortemente o conceito de fn de ordem superior [abstracao]
+	*/
+
+	// Dados para preencher a tabela
+	var MOUNTAINS = [
+		  {name: "Kilimanjaro", height: 5895, country: "Tanzania"},
+		  {name: "Everest", height: 8848, country: "Nepal"},
+		  {name: "Mount Fuji", height: 3776, country: "Japan"},
+		  {name: "Mont Blanc", height: 4808, country: "Italy/France"},
+		  {name: "Vaalserberg", height: 323, country: "Netherlands"},
+		  {name: "Denali", height: 6168, country: "United States"},
+		  {name: "Popocatepetl", height: 5465, country: "Mexico"}
+	];
+	function rowHeights (rows) {
+		return rows.map(function(row) {
+			return row.reduce(function(max, cell) {
+				return Math.max(max, cell.minHeight());
+			}, 0);
+		});
+	}
+
+	function colWidths (rows) {
+		return rows[0].map(function(_,i) { // variavel com '_' serve para humanos que o argumento não será usado
+			return rows.reduce(function(max, row) {
+				return Math.max(max, row[i].minWidth());
+			}, 0);
+		});
+	}
+
+	function drawTable(rows) {
+	 	var heights = rowHeights(rows);
+	 	var widths = colWidths(rows);
+
+		function drawLine(blocks, lineNo) {
+			return blocks.map(function(block) {
+			 	return block[lineNo];
+			}).join(" ");
+		}
+
+		function drawRow(row, rowNum) {
+			var blocks = row.map(function(cell, colNum) {
+			  	return cell.draw(widths[colNum], heights[rowNum]);
+		});
+		return blocks[0].map(function(_, lineNo) {
+		  	return drawLine(blocks, lineNo);
+		}).join("\n");
+	}
+
+	  return rows.map(drawRow).join("\n");
+	}
+
+	function repeat(string, times) {
+	 	var result = "";
+	 	for (var i = 0; i < times; i++)
+	    	result += string;
+	  	return result;
+	}
+
+	function TextCell(text) {
+	  	this.text = text.split("\n");
+	}
+	TextCell.prototype.minWidth = function() {
+	  	return this.text.reduce(function(width, line) {
+	    	return Math.max(width, line.length);
+	  	}, 0);
+	};
+	TextCell.prototype.minHeight = function() {
+	  	return this.text.length;
+	};
+	TextCell.prototype.draw = function(width, height) {
+	  	var result = [];
+	  	for (var i = 0; i < height; i++) {
+	    	var line = this.text[i] || "";
+	    	result.push(line + repeat(" ", width - line.length));
+	  	}
+	  	return result;
+	};
+
+	// testando
+	var rows = [];
+		for (var i = 0; i < 5; i++) {
+			var row = [];
+		   	for (var j = 0; j < 5; j++) {
+		     	if ((j + i) % 2 == 0)
+		       	row.push(new TextCell("##"));
+	     	else
+		       row.push(new TextCell("  "));
+		   }
+		   rows.push(row);
+		}
+	console.log(drawTable(rows));
+
+	// destaque para a linha do topo '---'
+	function UnderlinedCell(inner) {
+  		this.inner = inner;
+	};
+	UnderlinedCell.prototype.minWidth = function() {
+	  	return this.inner.minWidth();
+	};
+	UnderlinedCell.prototype.minHeight = function() {
+	  	return this.inner.minHeight() + 1;
+	};
+	UnderlinedCell.prototype.draw = function(width, height) {
+	  	return this.inner.draw(width, height - 1).concat([repeat("-", width)]);
+	};
+
+	// construindo a grade da tabela
+	function dataTable(data) {
+	  	var keys = Object.keys(data[0]);
+	  	var headers = keys.map(function(name) {
+	    	return new UnderlinedCell(new TextCell(name));
+  	});
+	  	var body = data.map(function(row) {
+	    	return keys.map(function(name) {
+	      	return new TextCell(String(row[name]));
+	    });
+  	});
+	  	return [headers].concat(body);
+	}
+
+	console.log(drawTable(dataTable(MOUNTAINS)));
+
+})();
